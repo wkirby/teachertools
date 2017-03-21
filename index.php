39a6ce9a0ed649177373afca1498d6e223b246ac
Wyatt Kirby <wyatt@apsis.io>
2017-03-21 11:50:15 -0700

    Move more templates to yaml files

--- index.php
+++ index.php
@@ -8,46 +8,21 @@ require_once('vendor/autoload.php');
 require_once('lib/worksheet_generator.cls.php');
 require_once('lib/problem_generator.cls.php');
 require_once('lib/addition_problem_generator.cls.php');
+require_once('lib/multiplication_problem_generator.cls.php');
 
-// Matches pattern of "x {{child}} in {{parent}}" or
-// "{{parent}} has x {{child}}"
-$noun_pairs = array(
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-  array("parent" => "field", "child" => "cows"),
-);
-
+// Fixtures
 $subjects = Yaml::parse(file_get_contents(dirname(__FILE__) . "/config/subjects.yml"));
 $objects = Yaml::parse(file_get_contents(dirname(__FILE__) . "/config/objects.yml"));
 
-$addition_templates = array(
-  "I have {{num_1}} {{object}}. {{subject}} has {{num_2}} {{object}}. How many {{object}} do we have all together?",
-  "{{subject}} has {{num_2}} {{object}}. I give {{subject_objective}} {{num_1}} {{object}}. How many does {{subject_subjective}} have now?"
-);
+// Templates
+$addTemplates = Yaml::parse(file_get_contents(dirname(__FILE__) . "/config/templates/addition.yml"));
+$subTemplates = Yaml::parse(file_get_contents(dirname(__FILE__) . "/config/templates/subtraction.yml"));
+$mulTemplates = Yaml::parse(file_get_contents(dirname(__FILE__) . "/config/templates/multiplication.yml"));
 
-$subtraction_templates = array(
-  "{{subject}} has {{num_2}} {{object}}. If {{subject_subjective}} gives me {{num_1}} {{object}}, how many {{object}} does {{subject_subjective}} have left?",
-  "I have {{num_2}} {{object}}. {{subject}} takes {{num_1}} {{object}}. How many {{object}} do I have left?",
-);
-
-// $multiplication_templates = array(
-//   "There are {{num_1}} {{object_1}}. There are {{num_2}} {{object_2}} in each {{object_1}}. How many {{object_2}} are there all together?",
-//   "I have {{num_1}} {{object_1}}. Each {{object_1}} has {{num_2}} {{object_2}}. How many {{object_2}} are there in total?",
-//   "There are {{num_2}} {{object_1}}. There are {{num_1}} {{object_2}} in each {{object_1}}. How many {{object_2}} are there all together?",
-//   "I have {{num_2}} {{object_1}}. Each {{object_1}} has {{num_1}} {{object_2}}. How many {{object_2}} are there in total?",
-// );
-
-$additionGenerator = new AdditionProblemGenerator($addition_templates);
-$subtractionGenerator = new AdditionProblemGenerator($subtraction_templates);
+// Generators
+$additionGenerator = new AdditionProblemGenerator($addTemplates);
+$subtractionGenerator = new AdditionProblemGenerator($subTemplates);
+$multiplicationGenerator = new MultiplicationProblemGenerator($mulTemplates);
 
 $additionGenerator
   ->setDigits(2, 3)
---
+++ lib/multiplication_problem_generator.cls.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Apsis;
+
+class MultiplicationProblemGenerator extends ProblemGenerator
+{
+  protected $objects;
+  protected $subjects;
+
+  protected $digitsA;
+  protected $digitsB;
+
+  public function setObjects($objects)
+  {
+    $this->objects = $objects;
+    return $this;
+  }
+
+  public function setSubjects($subjects)
+  {
+    $this->subjects = $subjects;
+    return $this;
+  }
+
+  public function setDigits($a, $b)
+  {
+    $this->digitsA = $a;
+    $this->digitsB = $b;
+
+    return $this;
+  }
+
+  function generateTemplateVars()
+  {
+    $subject = $this->subjects[array_rand($this->subjects)];
+    $object = $this->objects[array_rand($this->objects)];;
+
+    return array(
+      'num_1'    => $this->randomDigits($this->digitsA),
+      'num_2'    => $this->randomDigits($this->digitsB),
+      'object'   => $object,
+
+      'subject'            => $subject["subject"],
+      'subject_subjective' => $subject["pronouns"]["subjective"],
+      'subject_objective'  => $subject["pronouns"]["objective"],
+      'subject_possessive' => $subject["pronouns"]["possessive"]
+    );
+  }
+}
